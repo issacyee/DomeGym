8cd9fbacb73faed0403c6687cdb7fb5975294c48
yizijian99 <1350437095@qq.com>
2024-07-20 16:20:40 +0800

    feat: 初始化项目

---
+++ src/main/java/com/github/yizijian99/domegym/DomeGymApplication.java
@@ -0,0 +1,13 @@
+package com.github.yizijian99.domegym;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DomeGymApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DomeGymApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/github/yizijian99/domegym/DomeGymApplicationTests.java
@@ -0,0 +1,13 @@
+package com.github.yizijian99.domegym;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DomeGymApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
